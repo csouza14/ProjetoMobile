5da157b41f5bec93ab678b8ea7a25f299be33212
Camila Souza <camiilasouza@ymail.com>
2017-11-20 19:23:52 -0200

    Realizado algumas mudanças na interface.

---
+++ app/src/main/java/projetomobile/projetomobile/Activity/ImcActivity.java
@@ -0,0 +1,15 @@
+package projetomobile.projetomobile.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import projetomobile.projetomobile.R;
+
+public class ImcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
